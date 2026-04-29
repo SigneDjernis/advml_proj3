53c962187cab283bc34d5af0cedbf56bad04a5b6
bella <s214205@dtu.dk>
2026-04-29 20:56:20 +0200

    Baseline ER implementation

---
+++ baseline.py
@@ -0,0 +1,118 @@
+import torch
+from torch.utils.data import random_split
+from torch_geometric.datasets import TUDataset
+from torch_geometric.loader import DataLoader
+import numpy as np
+import networkx as nx
+
+device = 'cpu'
+
+dataset = TUDataset(root='./data/', name='MUTAG').to(device)
+node_feature_dim = 7
+
+# Split into training and validation
+rng = torch.Generator().manual_seed(0)
+train_dataset, validation_dataset, test_dataset = random_split(dataset, (100, 44, 44), generator=rng)
+train_loader = DataLoader(train_dataset, batch_size=100)
+
+
+class Baseline():
+
+    def __init__(self, dataloader):
+        
+        self.dataloader = dataloader
+        self.node_counts = self.count_nodes()
+        
+    def count_nodes(self):
+        all_node_counts = []
+
+        for data in self.dataloader:
+
+            counts = torch.bincount(data.batch)
+            all_node_counts.extend(counts.tolist())
+        
+        return np.array(all_node_counts)
+    
+    def sample_N(self):
+
+        values, counts = np.unique(self.node_counts, return_counts=True) # Return the different options for N and the counts
+        probs = counts / counts.sum()
+        N = np.random.choice(values, size=1, p=probs)
+        return N
+
+    def compute_link_prob(self):
+            
+            # we have data.edge_index: the index of two nodes which are connected
+            # we have data.batch: the graph index for each node
+
+            # First we find the elements of node_counts which are equal to N. Then we know the index of the graphs we are meant to explore
+            # Then we go through data.batch and get the nodes which belong to these graphs
+            # Then we count the edges the involves these nodes
+            # And we use N to count the number of possible edges and from that value count the number of possible edges
+            # At last we can compute the link probability   
+
+            N = self.sample_N()[0]
+            possible_edges_per_graph = N * (N - 1) / 2 # Number of edges in a complete graph
+
+            n_edges = 0
+            n_possible_edges = 0
+
+            for data in self.dataloader:
+
+                batch = data.batch
+                edge_index = data.edge_index
+
+                counts = torch.bincount(batch)
+
+                selected_graphs = (counts == N).nonzero(as_tuple=True)[0] # Get indices of graphs in the batch with N nodes
+                num_graphs = len(selected_graphs)
+
+                if num_graphs == 0:
+                    continue
+                
+                node_mask = torch.isin(batch, selected_graphs) # Find the nodes belonging to the selected graphs
+
+                edge_mask = node_mask[edge_index[0]] & node_mask[edge_index[1]]
+                num_edges = edge_mask.sum().item() # Count edges which connectes the nodes in the node_mask
+
+                n_edges += num_edges
+                n_possible_edges += num_graphs * possible_edges_per_graph
+
+            if n_possible_edges == 0:
+                return N, 0.0
+            
+            return N, n_edges / n_possible_edges
+
+                   
+    def erdos_reyi(self):
+
+        N, r = self.compute_link_prob()
+
+        # Generate each possible edge independently with a fixed probability
+
+        # using networkx
+        G = nx.erdos_renyi_graph(N, r)
+        adj = nx.adjacency_matrix(G).toarray()
+
+        # using torch
+        # upper = torch.rand((N, N))
+        # upper = (upper < r).int()
+        # upper = torch.triu(upper, diagonal=1)
+        # adj = upper + upper.T
+
+        return adj
+        
+
+# Task 1: Sample the number of nodes N from the emperical distribution of the number of nodes in the training data
+# i.e. emperical distribution over the number of nodes each graph has (graph size)
+# when generating a sample, randomly pick a value N with probabilities proportional to the height of the histogram
+
+# Task 2: Compute the link probability r  (number of edges divided by total possible number of edges) only using the graphs which have N nodes
+
+# Task 3: Sample a random graph with N nodes and edge probability r according to the Erdos-Renyi model
+
+base = Baseline(train_loader)
+
+sample = base.erdos_reyi()
+print(sample.shape)
+print(sample)
